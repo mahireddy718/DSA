2a01aafb7b9f75e01adce8787fc261ef4309787e
Dandhu Mahendra Reddy <149952771+mahireddy718@users.noreply.github.com>
2026-07-09 11:49:10 +0530

    Time: 0 ms (100%), Space: 8.5 MB (23.67%) - LeetHub

---
+++ 2351-first-letter-to-appear-twice/2351-first-letter-to-appear-twice.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    char repeatedCharacter(string s) {
+        unordered_map<char,int>mpp;
+        for(int i=0;i<s.length();i++){
+            mpp[s[i]]++;
+            if(mpp[s[i]]==2)return s[i];
+        }  
+        return -1;
+    }
+};
